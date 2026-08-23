7677f3e174334977319afdd752397e2a4227111c
adela320 <adacodrean@gmail.com>
2026-08-23 12:59:42 +0300

    added player name and chips; fixed new card button logic

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+let player = {
+    name : "Adela",
+    chips : 100
+}
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -7,6 +10,8 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard()
 {
@@ -59,8 +64,11 @@ function renderGame()
 
 function newCard()
 {
-    let card =  getRandomCard();
-    cards.push(card);
-    sum += card;
-    renderGame();
+    if(isAlive === true && hasBlackJack === false)
+    {
+        let card =  getRandomCard();
+        cards.push(card);
+        sum += card;
+        renderGame();
+    }
 }
